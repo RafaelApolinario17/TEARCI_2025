6230d54b33e0d7304705009fd2ef499374bc38fd
Rafael Apolinário <32022925+ljoggh17@users.noreply.github.com>
2025-10-17 10:03:41 +0100

    Add files via upload

---
+++ UC00670/SESSAO_17_10_2025/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+  pinMode(2,OUTPUT);
+
+}
+
+void loop() 
+{
+     digitalWrite(2,HIGH);
+     delay(500);
+     digitalWrite(2,LOW);
+     delay(500);
+}
